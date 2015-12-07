362e4ea152e6dc8c8b9d904a6b526da8aaae5e71
Stephen Best <bestie@gmail.com>
2015-12-07 23:30:28 +0000

    Update install script to include bin utils

--- install.sh
+++ install.sh
@@ -9,13 +9,27 @@ ln -s $working_dir/vimrc ~/.vimrc
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
+echo "Installing ackrc"
 ln -s $working_dir/ackrc ~/.ackrc
+echo "Installing gemrc"
 ln -s $working_dir/gemrc ~/.gemrc
+echo "Installing gitconfig"
 ln -s $working_dir/gitconfig ~/.gitconfig
+echo "Installing gitignore"
 ln -s $working_dir/gitignore ~/.gitignore
+echo "Installing gitprompt"
 ln -s $working_dir/gitprompt.sh ~/.gitprompt.sh
+echo "Installing inputrc"
 ln -s $working_dir/inputrc ~/.inputrc
+echo "Installing profile"
 ln -s $working_dir/profile ~/.profile
+echo "Installing ssh config"
 ln -s $working_dir/ssh/config ~/.ssh/config
+
+for f in $working_dir/bin/*
+do
+  echo "Installing $(basename $f)"
+  ln -s $f ~/bin
+done
 
 source ~/.profile
