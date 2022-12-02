861f4a1d47bcf2de09398760a5651a06b2df0b1e
Stephen Best <bestie@gmail.com>
2022-12-02 11:01:41 +0100

    IO.popen is actually much simpler!

--- ruby/reline_history_fzf_patch.rb
+++ ruby/reline_history_fzf_patch.rb
@@ -5,24 +5,13 @@ module RelineHistoryFzfPatch
       .map { IRB::Color.colorize_code(_1, ignore_error: true) }
       .join("\0")
 
-    stdin_read, stdin_write = IO.pipe
-    stdout_read, stdout_write = IO.pipe
-
-    stdin_write.write(history)
-    stdin_write.sync = true
-    stdin_write.close
-
     fzf_command = "fzf --no-info --no-sort --no-multi --ansi --read0 --scheme=history"
-    pid = spawn(fzf_command, in: stdin_read, out: stdout_write)
-    stdout_write.close
-    Process.wait(pid)
 
-    result = stdout_read.read
-    [stdin_read, stdin_write, stdout_read, stdout_write].each(&:close)
-
-    # If you would like an easy life, do this instead
-    # require "open3"
-    # result, _status = Open3.capture2(fzf_command, stdin_data: history)
+    result = nil
+    IO.popen(fzf_command, "r+") do |io|
+      io.write(history)
+      result = io.read
+    end
 
     @line_backup_in_history = whole_buffer
     @buffer_of_lines        = result.split("\n")
