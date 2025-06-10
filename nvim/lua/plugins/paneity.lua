967ea2a238d9af20828da3611ca0defb1df6ad0d
Stephen Best <bestie@gmail.com>
2025-06-10 21:46:43 +0200

    Paneity infer command WIP

--- nvim/lua/plugins/paneity.lua
+++ nvim/lua/plugins/paneity.lua
@@ -4,7 +4,7 @@ return {
 	dev = true,
   "bestie/paneity.nvim",
   config = function()
-    paneity = require("paneity")
+    local paneity = require("paneity")
     paneity.setup({
       marker = "🥖🔥",
       split_direction = "horizontal",
@@ -16,6 +16,39 @@ return {
       -- close_pane_on_exit
       -- default_command
     })
+
+    local guess_command = function()
+      local filetype = vim.bo.filetype
+
+      -- Makefile wins
+      if vim.fn.filereadable("Makefile") == 1 then
+        return "make"
+      end
+
+      if filetype == "ruby" then
+        local prefix = ""
+        if vim.fn.filereadable("Gemfile") == 1 then
+          prefix = "bundle exec "
+        end
+
+        -- is a an RSpec file? (ends in _spec.rb)
+        if vim.fn.expand("%:t"):match("_spec%.rb$") then
+          return prefix .. "rspec " .. vim.fn.expand("%")
+        else
+          return prefix .. "ruby " .. vim.fn.expand("%")
+        end
+      elseif filetype == "sh" or filetype == "bash" then
+        return "bash " .. vim.fn.expand("%")
+      -- Docker
+      elseif filetype == "dockerfile" then
+        return "docker build ."
+      -- C/C++
+      elseif filetype == "c" or filetype == "cpp" then
+        return "gcc " .. vim.fn.expand("%") .. " -o " .. vim.fn.expand("%:r") .. " && ./" .. vim.fn.expand("%:r")
+      else
+        return ""
+      end
+    end
 
     local save_then_repeat = function()
       vim.cmd("w!")
