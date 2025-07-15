e0365a0a86d2fae827295dbdd993c48c04e43eb0
Stephen Best <bestie@gmail.com>
2025-07-15 21:59:57 +0200

    Neotree tweaks

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -12,7 +12,7 @@ return {
       symbols = {
         -- Change type
         added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
-        modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+        modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
         deleted = "✖", -- this can only be used in the git_status source
         renamed = "󰁕", -- this can only be used in the git_status source
         -- Status type
@@ -41,8 +41,8 @@ return {
     filesystem = {
       filtered_items = {
         visible = false,
-        hide_dotfiles = true,
-        hide_gitignored = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
         position = "left",
       }
     }
